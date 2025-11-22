c51191abc610116a73a4adf3065f28de445d14f3
hyperpulseon <karthikcmegha@gmail.com>
2025-11-22 16:31:33 +0000

    Update BackgroundPattern mask to linear gradient fading to bottom

--- components/background-pattern.tsx
+++ components/background-pattern.tsx
@@ -18,7 +18,7 @@ export const BackgroundPattern = () => {
                 cy={1}
                 cr={1}
                 className={cn(
-                    "mask-[radial-gradient(ellipse,rgba(0,0,0,0.3)_30%,black_50%)]",
+                    "[mask-image:linear-gradient(to_bottom,white,transparent)]",
                     "dark:fill-slate-700"
                 )}
             />
